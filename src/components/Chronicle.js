9d2e100ee0b5a5f795dec557141f51fa8e3c7aaf
Zinstjerna <jltaoshirley@gmail.com>
2021-12-19 18:28:48 +0800

    修改样式

--- src/components/Chronicle.js
+++ src/components/Chronicle.js
@@ -1,27 +1,51 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import MaterialTable from 'material-table';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useTheme } from '@mui/material/styles';
 import { chronicleData, ageData } from './../datasets/ChronicleData.js';
 
 const columns = [
-    { field: 'age', title: '纪元', width: 10, lookup: ageData },
-    { field: 'time', title: '时期/代', width: 10 },
-    { field: 'date', title: '回', width: 10 },
-    { field: 'event', title: '大事件', width: 70 },
-    // { id: 'note', label: '备注', minWidth: 20 },
+    { field: 'age', title: '纪元', width: '7%', lookup: ageData },
+    {
+        field: 'time', title: '代', width: '10%', type: 'numeric',
+        customFilterAndSearch: (term, rowData) => parseInt(term) <= parseInt(rowData.time)
+    },
+    {
+        field: 'date', title: '回', width: '10%', type: 'numeric',
+        customFilterAndSearch: (term, rowData) => parseInt(term) <= parseInt(rowData.date)
+    },
+    { field: 'event', title: '大事件', width: '81%' }
 ];
 
 export default function Chronicle() {
+    const theme = useTheme();
     return (
         <Paper sx={{ margin: '1em 1em 0 1em' }}>
             <MaterialTable
+                style={{ color: 'inherit', backgroundColor: 'inherit' }}
                 title="初始世界编年史"
                 columns={columns}
                 data={chronicleData}
+                icons={{
+                    Filter: React.forwardRef((props, ref) => <FilterListIcon {...props} ref={ref} style={{ color: theme.palette.primary.contrastText }} />),
+                    Search: React.forwardRef((props, ref) => <ManageSearchIcon {...props} ref={ref} style={{ color: theme.palette.primary.contrastText }} />),
+                    ResetSearch: React.forwardRef((props, ref) => <ClearIcon {...props} ref={ref} style={{ color: theme.palette.primary.contrastText }} />)
+                }}
                 options={{
                     filtering: true,
                     paging: false,
-                    tableLayout: "fixed"
+                    tableLayout: "fixed",
+                    headerStyle: { color: 'inherit', backgroundColor: 'inherit' },
+                    filterCellStyle: { backgroundColor: theme.palette.primary.light },
+                    searchFieldStyle: { color: 'inherit', backgroundColor: 'inherit' }
+                }}
+                localization={{
+                    toolbar: {
+                        searchPlaceholder: '请输入……'
+                    }
                 }}
             />
         </Paper >
