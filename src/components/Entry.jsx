9a38765a7992442d611c9dc6ed99094991882081
Zinstjerna <jltaoshirley@gmail.com>
2021-12-23 15:16:23 +0800

    更新信息卡

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -9,6 +9,8 @@ import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
+import Chip from '@mui/material/Chip';
+import Divider from '@mui/material/Divider';
 import ReactMarkdown from 'react-markdown';
 import { parseMarkdown } from './../utils/utils.jsx';
 import useDataFetchAction from './../stores/actions/dataFetchAction.jsx';
@@ -22,12 +24,12 @@ export default function Entry() {
     const { state: info } = useDataFetchAction(ROOT_URL + 'cards/' + params.id + '.json', null);
 
     return (
-        <Grid container
+        <Grid container align='center'
             component={Paper}
             sx={{ margin: '1em 1em 1em 1em', padding: '0 1em 1em 0', width: 'auto' }}
             spacing={2}>
             <Grid item xs={12} sm={6} md={5} lg={4} xl={3}>
-                <Card sx={{ maxWidth: '400px' }}>
+                <Card align='left' sx={{ maxWidth: '400px' }}>
                     {figure.isLoading && <Skeleton variant="rectangular" width="auto" height={240} />}
                     {figure.isError && <CardMedia
                         component="img"
@@ -49,51 +51,74 @@ export default function Entry() {
                         {info.isError && <Typography gutterBottom variant="body" component="div">{params.id}信息卡为空，请干活！</Typography>}
                         {!info.isLoading && !info.isError && info.data &&
                             <>
-                            {
-                                info.data.name && <Typography gutterBottom align="center" variant="h6" component="div">{info.data.name}（{info.data.foreignName}）</Typography>
-                            }
-                            {
-                                info.data.nature && <Typography gutterBottom variant="body" component="p">本源：{info.data.nature}（{info.data.foreignNature}）</Typography>
-                            }
-                            {
-                                info.data.alias && <Typography gutterBottom variant="body" component="p">别名：{info.data.alias.join('、')}</Typography>
-                            }
-                            {
-                                info.data.born && <Typography gutterBottom variant="body" component="p"><ReactMarkdown children={parseMarkdown('出生：' + info.data.born.time + '，' + info.data.born.location)} /></Typography>
-                            }
+                                {
+                                    info.data.name && <Typography gutterBottom align="center" variant="h6" component="div">{info.data.name}（{info.data.foreignName}）</Typography>
+                                }
+                                {
+                                    info.data.nature && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('本源：' + info.data.nature + '（' + info.data.foreignNature + '）')} /></Typography>
+                                }
+                                {
+                                    info.data.alias && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('别名：' + info.data.alias.join('、'))} /></Typography>
+                                }
+                                {
+                                    info.data.ethnicity && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('种族：' + info.data.ethnicity.join('、'))} /></Typography>
+                                }
+                                {
+                                    info.data.nationality && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('民族：' + info.data.nationality.join('、'))} /></Typography>
+                                }
+                                <Divider sx={{ marginBottom: '0.25em' }} />
+                                {
+                                    info.data.born && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('出生：' + info.data.born.time + '，' + info.data.born.location)} /></Typography>
+                                }
+                                {
+                                    info.data.died && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('死亡：' + info.data.died.time + '，' + info.data.died.location)} /></Typography>
+                                }
+                                {
+                                    info.data.occupation && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('职业：' + info.data.occupation.join('、'))} /></Typography>
+                                }
+                                {
+                                    info.data.occupation && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('职业：' + info.data.occupation.join('、'))} /></Typography>
+                                }
+                                {
+                                    info.data.tools && <Typography gutterBottom variant="body2" component="div"><ReactMarkdown children={parseMarkdown('器具：' + info.data.tools.join('、'))} /></Typography>
+                                }
+                                <Divider sx={{ marginBottom: '0.25em' }} />
+                                {
+                                    info.data.height && <Typography gutterBottom variant="body2" component="div">身高：{info.data.height}</Typography>
+                                }
+                                {
+                                    info.data.weight && <Typography gutterBottom variant="body2" component="div">体重：{info.data.weight}</Typography>
+                                }
+                                {
+                                    info.data.eyeColor && <Typography gutterBottom variant="body2" component="div">瞳色：
+                                        {
+                                            info.data.eyeColor.normal &&
+                                            <span>
+                                                 常态 <Chip 
+                                                 label={info.data.eyeColor.normal.name} 
+                                                 style={{backgroundColor: info.data.eyeColor.normal.code, height: '1.5em'}} />
+                                            </span>
+                                        }
+                                        {
+                                            info.data.eyeColor.disguise &&
+                                            <span>
+                                                 | 伪装 <Chip 
+                                                 label={info.data.eyeColor.disguise.name} 
+                                                 style={{backgroundColor: info.data.eyeColor.disguise.code, height: '1.5em'}} />
+                                            </span>
+                                        }
+                                    </Typography>
+                                }
                             </>
                         }
                     </CardContent>
-                    
-
-
-                        <Typography gutterBottom variant="h5" component="div">
-                            {params.id}
-                        </Typography>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            隶属：上古神祗
-                        </Typography>
-                        <Typography variant="body1" color="text.primary">
-                            性别：女
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            出生时间：【上古纪】
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            死亡时间：-
-                        </Typography>
-                        <Typography variant="body2">
-                            职务：神祗、开天辟地之人、初始之初
-                        </Typography>
-
-                    <CardActions>
+                    {/* <CardActions>
                         <Button size="small">分享</Button>
                         <Button size="small">查看更多</Button>
-                    </CardActions>
-
+                    </CardActions> */}
                 </Card>
             </Grid>
-            <Grid item xs={12} sm={6} md={7} lg={8} xl={9}>
+            <Grid item align='left' xs={12} sm={6} md={7} lg={8} xl={9}>
                 {article.isLoading && <Typography variant="body" component="div">正在加载……</Typography>}
                 {article.isError && <Typography variant="body" component="div">暂无内容，请努力耕耘。</Typography>}
                 {!article.isLoading && !article.isError && <ReactMarkdown children={parseMarkdown(article.data)} />}
